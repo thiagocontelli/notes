f7362fd5d9cc5dde56bb302dfa9fe6092a8f821c
thiagocontelli <thiagocontellid@gmail.com>
2023-06-28 12:16:54 -0300

    feat: add app bar to note screen

--- app/src/main/java/com/thiagocontelli/notes/fragments/NoteFragment.kt
+++ app/src/main/java/com/thiagocontelli/notes/fragments/NoteFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.thiagocontelli.notes.R
 import com.thiagocontelli.notes.databinding.FragmentNoteBinding
 
 class NoteFragment : Fragment() {
@@ -21,8 +24,31 @@ class NoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.titleTextView.text = arguments?.getString("title")
-        binding.createdAtTextView.text = arguments?.getString("createdAt")
+        binding.topAppBar.title = arguments?.getString("title")
+        binding.topAppBar.subtitle = arguments?.getString("createdAt")
         binding.contentTextView.text = arguments?.getString("content")
+
+        binding.topAppBar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.topAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.delete_note -> {
+                    context?.let { ctx ->
+                        MaterialAlertDialogBuilder(ctx).setTitle("Delete Note")
+                            .setMessage("Are you sure you want to delete this note? This action cannot be undone.")
+                            .setNegativeButton("Cancel") { dialog, _ ->
+                                dialog.cancel()
+                            }.setPositiveButton("Delete") { _, _ ->
+                                findNavController().popBackStack()
+                            }.show()
+                    }
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
