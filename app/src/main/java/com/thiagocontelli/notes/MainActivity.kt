ae4d8be4199bdb113464b176d0adc7dc512b4cca
thiagocontelli <thiagocontellid@gmail.com>
2023-06-28 09:29:18 -0300

    initial commit

---
+++ app/src/main/java/com/thiagocontelli/notes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thiagocontelli.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
