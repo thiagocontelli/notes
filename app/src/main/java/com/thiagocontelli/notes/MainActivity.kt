9834ce026f2ecfdfbb10cb25961018036c879f63
thiagocontelli <thiagocontellid@gmail.com>
2023-06-28 09:34:22 -0300

    chore: change to view binding

--- app/src/main/java/com/thiagocontelli/notes/MainActivity.kt
+++ app/src/main/java/com/thiagocontelli/notes/MainActivity.kt
@@ -2,10 +2,14 @@ package com.thiagocontelli.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thiagocontelli.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
