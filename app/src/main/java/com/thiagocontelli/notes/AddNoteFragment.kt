dd2686062b10cd1b5e307488a1e8ea1f461e2dd8
thiagocontelli <thiagocontellid@gmail.com>
2023-06-28 09:49:04 -0300

    feat: implement navigation component

---
+++ app/src/main/java/com/thiagocontelli/notes/AddNoteFragment.kt
@@ -0,0 +1,20 @@
+package com.thiagocontelli.notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.thiagocontelli.notes.databinding.FragmentAddNoteBinding
+
+class AddNoteFragment : Fragment() {
+    private var _binding: FragmentAddNoteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/thiagocontelli/notes/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.thiagocontelli.notes
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.thiagocontelli.notes.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
