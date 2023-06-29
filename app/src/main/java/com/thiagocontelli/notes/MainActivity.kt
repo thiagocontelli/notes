34b3c94e67819ceeadadd7f34d7125467c28be32
thiagocontelli <thiagocontellid@gmail.com>
2023-06-29 12:00:14 -0300

    chore: set light theme only

--- app/src/main/java/com/thiagocontelli/notes/MainActivity.kt
+++ app/src/main/java/com/thiagocontelli/notes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.thiagocontelli.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.thiagocontelli.notes.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(binding.root)
     }
 }
